673a099efceb0e2c53a3f7e1a43346bc5a8518a2
vovapankiw <vovapankiw@gmail.com>
2021-02-25 10:01:56 +0200

    added base route for todo

--- front-client/src/app/app-routing.module.ts
+++ front-client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppTodoList } from './list/app-todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'todo-list', component:  AppTodoList },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
