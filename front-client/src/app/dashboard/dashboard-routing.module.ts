5c85fa858708c8b19e7d16c10c8e96d86ffbec32
vovapankiw <vovapankiw@gmail.com>
2021-03-13 20:22:01 +0200

    added load children for todo item

--- front-client/src/app/dashboard/dashboard-routing.module.ts
+++ front-client/src/app/dashboard/dashboard-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppTodoList } from '../todo/list/app-todo-list.component';
 import { DashboardComponent } from './dashboard.component';
+import { TodoRoutingModule } from '../todo/todo-routing.module';
 
 const routes: Routes = [
   { path: 'dashboard', component:  DashboardComponent,
@@ -9,6 +10,7 @@ const routes: Routes = [
       {
         path: 'todo-list',
         component: AppTodoList,
+        loadChildren: () => TodoRoutingModule
       }
     ]
   }
