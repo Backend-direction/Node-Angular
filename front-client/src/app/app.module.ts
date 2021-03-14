70f8953647aa5db8005d2578dcf43540fc1e0f32
vovapankiw <vovapankiw@gmail.com>
2021-03-14 09:43:12 +0200

    change imports for material

--- front-client/src/app/app.module.ts
+++ front-client/src/app/app.module.ts
@@ -4,13 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AuthModule } from './auth/auth.module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DialogComponent } from './common/dialog/dialog.component';
-
-import {MatDialogModule} from '@angular/material/dialog';
 import { PageNotFoundComponent } from './common/page-not-found/page-not-found.component';
 
 @NgModule({
@@ -27,7 +26,7 @@ import { PageNotFoundComponent } from './common/page-not-found/page-not-found.co
     FormsModule,
     AuthModule,
     BrowserAnimationsModule,
-    MatDialogModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
